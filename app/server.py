6bbc97fb939bd6b7338587a57e7e9210901b18c5
yehuda744 <56075433+yehuda744@users.noreply.github.com>
2019-11-03 14:29:19 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1JkGvrx0hiYSKNZDqNUf8ao1kgsWfhj3'
+export_file_url = 'https://www.dropbox.com/s/rmgj0paikmkvksz/lesson2-download-Copy2.ipynb?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['men', 'women']
