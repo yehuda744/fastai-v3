cf5c9d7d3b2ce609f41e88d2e1e174205d55a70f
yehuda744 <56075433+yehuda744@users.noreply.github.com>
2019-11-04 16:04:43 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/derklcvjfcq7vlm/export.pkl?dl=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1OOCrNzwTSoGsEsR-wc5yrVWfujnhbTR8'
 export_file_name = 'export.pkl'
 
 classes = ['men', 'women']
